6344c4ac06568e7f28b2bd2f32889c5b33c34dcd
Bogdan3690 <dblevko@gmail.com>
2026-01-28 14:55:59 -0700

    done second problem

--- script.js
+++ script.js
@@ -12,13 +12,45 @@ const pictureObserver = new IntersectionObserver(onEntry, options)
 
 function onEntry(entries, observer){
     entries.forEach(entry => {
-        console.log(entry);
-        
+        // console.log(entry);
+        if (entry.isIntersecting) {
+            entry.target.classList.add('appear')
+        }else{
+            entry.target.classList.remove('appear')
+        }
     })
 }
 
 
 images.forEach(image => {
-    
 pictureObserver.observe(image)
 })
+
+
+
+
+
+
+
+
+
+
+
+
+const item = document.querySelector(".feed__item img")
+// console.log(item);
+
+const lazyObserver = new IntersectionObserver(onImgEntry, options)
+
+function onImgEntry(entries, observeer){
+    entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+            const data = entry.target.dataset.lazy
+            entry.target.src = data
+            entry.target.classList.add('appear')
+            lazyObserver.unobserve(item)
+        }
+    })
+}
+
+lazyObserver.observe(item)
